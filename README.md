8afa519bb2a3b6950cf66717002fca191d9534e3
The-Ufuoma <61988196+The-Ufuoma@users.noreply.github.com>
2020-03-26 23:03:39 +0100

    Update and rename README.md to index.js

--- README.md
+++
@@ -1,2 +0,0 @@
-# First-Javascript-Task
-first Javascript task @start.ng
---
+++ index.js
@@ -0,0 +1,21 @@
+# First-Javascript-Task
+first Javascript task @start.ng
+var myName;
+myName= "The-Ufuoma"
+var registeredCourses;
+registeredCourses= ["HTML", "CSS", "Javascript", "UI/UX/Graphics"]
+
+console.log(myName);
+console.log(registeredCourses);
+
+const data = [];
+
+ for (let i=0; i<=200; i++) {
+
+   if (i%2===0) {
+
+     data.push(i);
+
+   }
+
+ }console.log(data);
